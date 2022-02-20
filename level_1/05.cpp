e5cbefbebd35f1731917195f9cdffeb58d28606a
ggh-png <0380089@gmail.com>
2022-02-20 15:28:00 +0900

    직사각형 별찍기

---
+++ level_1/05.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+     int n =0;
+     int m =0;
+    
+     cin >> n >> m;
+     for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            cout << "*";
+        }
+            cout << endl;
+    }
+	
+}
