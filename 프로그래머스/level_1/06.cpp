d064cce51fedc08c39ff15f576f4ad6ff573219a
ggh-png <0380089@gmail.com>
2022-02-21 23:18:54 +0900

    중복만 해결하면 댐

--- 프로그래머스/level_1/06.cpp
+++ 프로그래머스/level_1/06.cpp
@@ -2,12 +2,11 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include <algorithm>
 
 using namespace std;
 
 
-void splitAndEnterReportList(vector<string> id_list, vector<string> report, int limit)
+vector<int> solution(vector<string> id_list, vector<string> report, int limit)
 {
     // split
     vector<string> temp;
@@ -15,6 +14,7 @@ void splitAndEnterReportList(vector<string> id_list, vector<string> report, int 
     vector<vector<bool>> reportList(id_list.size(), vector<bool>(id_list.size()));
     vector<int> reportSate(id_list.size());
     vector<int> answer(id_list.size(), 0);
+    vector<int> list(id_list.size(), 0);
 
     for(int i=0; i <= id_list.size(); i++)
     {
@@ -26,6 +26,7 @@ void splitAndEnterReportList(vector<string> id_list, vector<string> report, int 
         // 신고자 무지, 신고대상 프로도 
 
         for(int j=0; j < id_list.size(); j++)
+        {
             if(temp[0] == id_list[j]) // 신고자를 id리스트에서 찾으면 
            {// j = reporter, k = report
                 for(int k=0; k < id_list.size(); k++)
@@ -33,64 +34,57 @@ void splitAndEnterReportList(vector<string> id_list, vector<string> report, int 
                     if(temp[1] == id_list[k]) // 신고대상 
                     {
                         reportList[j][k] = true; // reportList[][]에 저장 (신고 리스트 등록)
-                        reportSate[k] += 1;   // reportSate에 스택 업 (신고 현황 업데이트)
-                        answer[j] += (reportSate[k] >= limit) ? 1 : 0;  
                     }
-
-
-
-                    // else if(reportSate[k] >= 2)//신고가 누적되면
-                    // {
-                    //     answer[j] += 1; // 메일발송 
-                    //     //break;
-                    // }
                 }
             }
-            // --------------------------------------------------
+        }
     }
-// 신고상황 출력   
-    for(auto el : reportSate)
-    {
-        cout << el << " ";
-    }
-s
-// 신고 기록 출력
-    cout << endl << endl << "reportList" << endl << endl;
+
+
 
     for(int i=0; i < id_list.size(); i++)
     {
         for(int j=0; j < id_list.size(); j++)
         {
-            cout << reportList[i][j] << " ";
-            
+            if(reportList[i][j]) // 중복일 경우 3번 투표해도 1번으로 
+                reportSate[j] += 1; 
         }
-        cout << endl; 
     }
 
-    for(auto &el : answer)
+    for(int i=0; i < id_list.size(); i++)
     {
-        cout << el << " ";
+        if(reportSate[i] >= limit)
+        {
+            list.push_back(i);
+        }
     }
-    cout << endl;
+
+    for(int i = 0; i < id_list.size(); i++)
+    {
+        for(auto el : list)
+        {
+            if(reportList[i][el])
+            {
+                answer[i] += 1;
+            }
+        }
+    }
+
+    return answer;
 }
 
-// vector<int> solution(vector<string> id_list, vector<string> report, int k)
-// {
-//     splitAndEnterReportList(id_list, report, k);
-//     for(auto &el : repor)
-// }
+
 
 
 int main()
 
 {
-    vector<string> id_list{"muzi", "frodo", "apeach", "neo"};
-    vector<string> report{ "muzi frodo","apeach frodo","frodo neo",
-		"muzi neo","apeach muzi" };
-    int k = 2;
+    vector<string> id_list{"con", "ryan"};
+    vector<string> report{ "ryan con", "ryan con", "ryan con", "ryan con"};
+    int k = 3;
 
     //solution(id_list, report, k);
-    splitAndEnterReportList(id_list, report, k);
+    solution(id_list, report, k);
     //cout << "asda" << endl;
     return 0;    
 
