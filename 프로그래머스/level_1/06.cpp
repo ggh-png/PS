f1e57119459c30e674bd5186ec55d7ec5be0fdf4
ggh-png <0380089@gmail.com>
2022-02-21 16:05:10 +0900

    마지막 이메일 보내는것만 구현하면 끝남

--- 프로그래머스/level_1/06.cpp
+++ 프로그래머스/level_1/06.cpp
@@ -6,16 +6,15 @@
 
 using namespace std;
 
-//vector<vector<bool>> reportList; // 신고횟수  [0 1 0 1][0 0 0 1][1 1 0 0][0 0 0 0]
 
-
-void splitAndEnterReportList(vector<string> id_list, vector<string> report, int k)
+void splitAndEnterReportList(vector<string> id_list, vector<string> report, int limit)
 {
     // split
     vector<string> temp;
     string stringBuffer;
     vector<vector<bool>> reportList(id_list.size(), vector<bool>(id_list.size()));
     vector<int> reportSate(id_list.size());
+    vector<int> answer(id_list.size());
 
     for(int i=0; i <= id_list.size(); i++)
     {
@@ -25,40 +24,50 @@ void splitAndEnterReportList(vector<string> id_list, vector<string> report, int 
             temp.push_back(stringBuffer);
         //"temp[0] = muzi, tem[1] = frodo"
         // 신고자 무지, 신고대상 프로도 
-        for(int k=0; k < id_list.size(); k++)
-            if(temp[0] == id_list[k]) // 신고자를 id리스트에서 찾으면 
-           {
-                for(int j=0; j < id_list.size(); j++)
+
+        for(int j=0; j < id_list.size(); j++)
+            if(temp[0] == id_list[j]) // 신고자를 id리스트에서 찾으면 
+           {// j = reporter, k = report
+                for(int k=0; k < id_list.size(); k++)
                 {
-                    if(temp[1] == id_list[j]) // 신고대상 
+                    if(temp[1] == id_list[k]) // 신고대상 
                     {
-                        reportList[k][j] = true; // reportList[][]에 저장 (신고 리스트 등록)
-                        reportSate[j] += 1;   // reportSate에 스택 업 (신고 현황 업데이트)
+                        reportList[j][k] = true; // reportList[][]에 저장 (신고 리스트 등록)
+                        reportSate[k] += 1;   // reportSate에 스택 업 (신고 현황 업데이트)
+                    }
+                    else if(reportSate[k] >= limit)
+                    {
+                        answer[j] += 1;
+                        //break;
                     }
                 }
             }
+            // --------------------------------------------------
+    }
+// 신고상황 출력   
+    for(auto el : reportSate)
+    {
+        cout << el << " ";
+    }
 
+// 신고 기록 출력
+    cout << endl << endl << "reportList" << endl << endl;
+
+    for(int i=0; i < id_list.size(); i++)
+    {
+        for(int j=0; j < id_list.size(); j++)
+        {
+            cout << reportList[i][j] << " ";
             
-        for(auto el : reportSate)
-        {
-            cout << el << " ";
         }
+        cout << endl; 
+    }
 
-
-        cout << endl << endl << "reportList" << endl;
-
-        for(int i=0; i < id_list.size(); i++)
-        {
-            for(int j=0; j < id_list.size(); j++)
-            {
-                cout << reportList[i][j] << " ";
-            }
-            cout << endl; 
-        }
-        
-        
-        
-    }   
+    for(auto &el : answer)
+    {
+        cout << el << " ";
+    }
+    cout << endl;
 }
 
 // vector<int> solution(vector<string> id_list, vector<string> report, int k)
