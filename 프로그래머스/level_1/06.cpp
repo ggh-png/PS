4bf8b730101894cb3de1383f9726e9042a22bed3
ggh-png <0380089@gmail.com>
2022-02-21 13:19:18 +0900

    splitAndEnterReportList

--- 프로그래머스/level_1/06.cpp
+++ 프로그래머스/level_1/06.cpp
@@ -1,75 +1,59 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <map>
 #include <sstream>
 #include <algorithm>
 
 using namespace std;
 
-map<string, int> user;
 //vector<vector<bool>> reportList; // 신고횟수  [0 1 0 1][0 0 0 1][1 1 0 0][0 0 0 0]
 
 
 void splitAndEnterReportList(vector<string> id_list, vector<string> report, int k)
 {
-   // split
-   vector<string> temp;
-   string stringBuffer;
-   vector<vector<bool>> reportList(id_list.size(), vector<bool>(id_list.size()));
+    // split
+    vector<string> temp;
+    string stringBuffer;
+    vector<vector<bool>> reportList(id_list.size(), vector<bool>(id_list.size()));
+    vector<int> reportSate(id_list.size());
 
-    for(int i=0; i < report.size(); i++)
+    temp.clear(); // 초기화
+
+    istringstream ss(report[0]);
+    while (getline(ss, stringBuffer, ' '))
+        temp.push_back(stringBuffer);
+    //"temp[0] = muzi, tem[1] = frodo"
+    // 신고자 무지, 신고대상 프로도 
+
+    if(temp[0] == id_list[0]) // 신고자를 id리스트에서 찾으면 
     {
-        temp.clear(); // 초기화
-        // split
-        istringstream ss(report[0]);
-        while (getline(ss, stringBuffer, ' '))
-            temp.push_back(stringBuffer);
-        //"muzi frodo"
-    }
-    
-
-    //reportList[][]에 저장
-    
-    for(int j=0; j < id_list.size(); j++)
-    {   // reporter를 찾으면
-        if(find(temp.begin(), temp.end(), id_list[j]) == temp.begin())
-        // report 해라
+        reportList[0][1] = true; // reportList[][]에 저장 (신고 리스트 등록)
+        reportSate[1] += 1;   // reportSate에 스택 업 (신고 현황 업데이트)
+        for(auto el : reportSate)
         {
-            //cout << "asdasdasd" << endl;
-            for(int k=0; k < id_list.size(); k++)
-            {
-                if(find(temp.begin(), temp.end(), id_list[k]) != temp.end())
-                    reportList[j][k] = 1;
-            }
-        } 
-        
-    }
-    for(int j=0; j < id_list.size(); j++)
-    {
-        for(auto el : reportList[0])
             cout << el << " ";
+        }
         cout << endl;
-    }
-
-
-
-    //cout << temp[0] << " " <<  temp[1] << endl;
-
+    }   
 }
 
+// vector<int> solution(vector<string> id_list, vector<string> report, int k)
+// {
+//     splitAndEnterReportList(id_list, report, k);
+//     for(auto &el : repor)
+// }
 
 
 int main()
 
 {
     vector<string> id_list{"muzi", "frodo", "apeach", "neo"};
-    vector<string> report{"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+    vector<string> report{"muzi frodo"};
     int k = 2;
 
     //solution(id_list, report, k);
     splitAndEnterReportList(id_list, report, k);
-    cout << "asda" << endl;
+    //cout << "asda" << endl;
     return 0;    
 
 }
