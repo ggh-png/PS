f353eb943a733f2c06c461168dfc35648e6c2576
ggh-png <0380089@gmail.com>
2022-02-26 16:38:04 +0900

    완주하지 못한 선수

--- 프로그래머스/level_1/02.cpp
+++ 프로그래머스/level_1/02.cpp
@@ -1,53 +1,33 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include <algorithm>
 
 using namespace std; 
- 
 
-string solution (string participant[], string completion[],int participant_size, int completion_size);
+string solution (vector<string> participant, vector<string> completion)
+{
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+
+    for(int i=0; i<participant.size(); ++i)
+    {
+        if(participant[i] != completion[i])
+            return participant[i];
+        //cout << participant[i] << " ";
+    }
+    for(auto el : participant)
+        cout << el << " ";
+    cout << endl;
+    return participant[participant.size() - 1];
+
+}
 
 int main()
 {
-    string participant[] = {"kiki", "eden", "kiki"};           //참가자 
-	string completion[] = { "kiki", "eden" };                   //완주자 
+    vector<string> participant = {"mislav", "stanko", "mislav", "ana"};           //참가자 
+	vector<string> completion = {"stanko", "ana", "mislav"};                   //완주자 
 
-    int participant_size = sizeof(participant)/sizeof(string);  
-    int completion_size = sizeof(completion)/sizeof(string);    //참가자 완주자 수 
-
-    solution(participant, completion, participant_size, completion_size);
+    cout << solution(participant, completion) << endl;
     return 0;
-}
-
-
-
-string solution (string participant[], string completion[],int participant_size, int completion_size)
-{
-
-    string str_check = "null";
-    string answer;
-
-
-    for(int i=0; i < participant_size; i++)// {"kiki", "eden", "kiki"}; 
-    {
-        for(int j=0; j < completion_size; j++) //{ "kiki", "eden" 
-        {
-            if (participant[i].compare(completion[j])==0)
-            {
-                participant[i] = str_check;
-                completion[j] = str_check;               
-            }           
-        }
-    }
-
-    for(int k=0; k < participant_size; k++)
-    {
-        
-        if(str_check.compare(participant[k]) != 0) 
-        {
-            answer = participant[k];
-        }
-    }
-    cout << answer << endl;
-    return answer;
 }
