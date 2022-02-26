f2a53dfb864d9df5fea56234fcd7ec5b5e81f6df
ggh-png <0380089@gmail.com>
2022-02-26 15:13:52 +0900

    같은 숫자는 싫어

--- 프로그래머스/level_1/03.cpp
+++ 프로그래머스/level_1/03.cpp
@@ -4,68 +4,12 @@
 
 using namespace std;
 
-int solution (int arr[], int arr_size);
-void print_arr (int arr[], int arr_size);
-int output_arr_size(int arr[], int arr_size);
 
 
-int main() 
-{
-    
-    int arr[] = {1,5,1,5,3}; // 151
-    int arr_size = sizeof(arr) / sizeof(int);
-    solution(arr, arr_size);
-    
-	return 0;
-}
-
-int solution (int arr[], int arr_size)
-{
-
-    int size = output_arr_size(arr, arr_size);
-    int output_arr[size];
-    int temp_arr=0;
-
-
-    for(int i=0; i < arr_size; i++)/// 133133 -- 6 arr size
-    {                               // 1313 --   4 size
-
-        if((arr[i] == arr[i+1]) ? false : true)                 // 앞 뒤가  같으면 
-        {
-            if(i+2  > arr_size)
-            {
-                output_arr[size-1] = arr[arr_size-1];
-               // cout << "test  : " << output_arr[size] << endl;
-                
-                break;
-            }
-            temp_arr = arr[i];// 새로운 배열에 순서대로 초기화 // 1 31333
-            
-
-            for(int j=i; j <= size; j++)  
-            {
-                output_arr[j] = temp_arr;            
-            }     
-        }
-        else
-        {
-            temp_arr = arr[i+2];
-            for(int j=i; j <= size; j++)
-            {
-                output_arr[j] = temp_arr; 
-            }
-        }   
-    } 
-    print_arr (output_arr, size);
-    return 0;
-}
-
-
-
-int output_arr_size(int arr[], int arr_size)
+int answer_size(vector<int> arr)
 {   
     int count = 0;
-    for(int i=0; i < arr_size; i++)
+    for(int i=0; i < arr.size(); i++)
     {
         if((arr[i] == arr[i+1]) ? false : true) // 311331
         {
@@ -78,12 +22,69 @@ int output_arr_size(int arr[], int arr_size)
 
 
 
-void print_arr (int arr[], int arr_size)
+void print_arr (vector<int> arr)
 {
     cout << "[ ";
-    for(int i=0; i < arr_size; i++)
+    for(int i=0; i < arr.size(); i++)
     {
         cout << arr[i] << ", ";
     }
     cout << "]" << endl;
+}
+
+
+
+
+vector<int> solution (vector<int> arr)
+{
+
+    int size = answer_size(arr);
+    vector<int> answer(size);
+    int temp_arr=0;
+
+
+    for(int i=0; i < arr.size(); i++)/// 133133 -- 6 arr size
+    {                               // 1313 --   4 size
+
+        if((arr[i] == arr[i+1]) ? false : true)                 // 앞 뒤가  같으면 
+        {
+            if(i+2  > arr.size())
+            {
+                answer[size-1] = arr[arr.size()-1];
+               // cout << "test  : " << answer[size] << endl;
+                
+                break;
+            }
+            temp_arr = arr[i];// 새로운 배열에 순서대로 초기화 // 1 31333
+            
+
+            for(int j=i; j <= size; j++)  
+            {
+                answer[j] = temp_arr;            
+            }     
+        }
+        else
+        {
+            temp_arr = arr[i+2];
+            for(int j=i; j <= size; j++)
+            {
+                answer[j] = temp_arr; 
+            }
+        }   
+    } 
+    print_arr (answer);
+    return answer;
+}
+
+
+
+
+int main() 
+{
+    
+    vector<int> arr = {1, 1, 3, 3, 0, 1, 1}; // 151
+    int arr_size = sizeof(arr) / sizeof(int);
+    solution(arr);
+    
+	return 0;
 }
