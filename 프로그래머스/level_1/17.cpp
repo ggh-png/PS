486964024fe8f4ce6a918520aff804b893717bec
ggh-png <0380089@gmail.com>
2022-03-01 15:07:54 +0900

    행렬의 덧셈

---
+++ 프로그래머스/level_1/17.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) 
+{
+    vector<vector<int>> answer(arr1.size(),vector<int>(arr1[0].size()));
+    for(int i=0; i < arr1.size(); i++)
+        for(int j=0; j < arr2.size(); j++)
+            answer[i][j] = arr1[i][j] + arr2[i][j];
+
+    return answer;
+}
+
+
+
+int main()
+{
+    return 0;
+}
