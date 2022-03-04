6536b7a1dad07ba7c1e05c018da672e53d4e40c4
ggh-png <0380089@gmail.com>
2022-03-04 16:10:54 +0900

    정수 내림차순으로 배치하기

---
+++ 프로그래머스/level_1/25.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) 
+{
+    long long answer = 0;
+    vector<int> temp;
+    while (n > 0)
+    {
+        temp.push_back(n%10);
+        n /= 10;
+    }
+    sort(temp.begin(), temp.end());
+
+    for(int i=0; i < temp.size(); i++)
+        answer += pow(10, i)*temp[i];    
+    return answer;
+
+}
+
+int main()
+{
+    long long n = 118372;
+    solution(n);
+    return 0; 
+}
