81950b9429c72ede365a92235e6975037a50f014
ggh-png <0380089@gmail.com>
2022-03-06 21:47:34 +0900

    문자열 내 마음대로 정렬하기

---
+++ 프로그래머스/level_1/33.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// n번째 문자열을 기준으로 오름차순 정렬 
+// n번째 문자 추출 
+// 추출된 문자에 번호 부여 
+
+vector<string> solution(vector<string> strings, int n) 
+{
+    vector<string> answer;
+    vector<char> temp; 
+    sort(strings.begin(), strings.end());
+
+    for(int i=0; i < strings.size(); i++)   
+        temp.push_back(strings[i][n]);
+    sort(temp.begin(), temp.end());
+
+    for(int i=0; i < strings.size(); i++)
+    {
+        for(int j=0; j < temp.size(); j++)
+        {
+            if(temp[i] == strings[j][n])
+            {
+                answer.push_back(strings[j]);
+                strings[j] = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";    
+            }
+                
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    vector<string> strings{"abce", "abcd", "cdx"};
+    int n = 2;
+    solution(strings, n);
+    return 0;
+}
