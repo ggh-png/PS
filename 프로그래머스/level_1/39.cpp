a40a0af96d99314eb01c374109ef48986bbc5d38
ggh-png <0380089@gmail.com>
2022-03-11 12:09:04 +0900

    소수 만들기

---
+++ 프로그래머스/level_1/39.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+// 1. 3개의 요소를 뽑는다.
+// 2. 뽑은 요소를 넣는다.
+// 3. 
+
+int solution(vector<int> nums) 
+{
+    int answer = -1;
+    vector<int> temp;
+    for(int i=0; i < nums.size(); i++)
+        for(int j=0; j < nums.size(); j++)
+            for(int k=0; k < nums.size(); k++)
+            {
+                
+            }
+
+    return answer;
+}
+
+
+int main()
+{
+
+    return 0; 
+}
