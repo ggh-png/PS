79450faf9fdb8c638ba9efc3226aa053dee7cd2b
ggh-png <0380089@gmail.com>
2022-03-11 19:32:45 +0900

    소수 만들기

--- 프로그래머스/level_1/39.cpp
+++ 프로그래머스/level_1/39.cpp
@@ -1,27 +1,31 @@
 #include <vector>
 #include <iostream>
 using namespace std;
-// 1. 3개의 요소를 뽑는다.
-// 2. 뽑은 요소를 넣는다.
-// 3. 
 
 int solution(vector<int> nums) 
 {
-    int answer = -1;
+    int answer = 0;
     vector<int> temp;
     for(int i=0; i < nums.size(); i++)
-        for(int j=0; j < nums.size(); j++)
-            for(int k=0; k < nums.size(); k++)
-            {
-                
-            }
-
+        for(int j=i+1; j < nums.size(); j++)
+            for(int k=j+1; k < nums.size(); k++) 
+                temp.push_back(nums[i] + nums[j] + nums[k]);
+                // 모든 경우의 수 입력
+    // 중복된 요소제거 
+    for(int i=0; i < temp.size(); i++)
+    {
+        int temp_num = 0;
+        for(int j=2; j < temp[i]; j++) // 소수인지 판별 // 최소 3 
+            temp_num += (temp[i] % j) == 0 ? true : false;
+        answer += (temp_num == 0) ? true : false;
+    }
+    // 소수 판별 
     return answer;
 }
 
-
 int main()
 {
-
+    vector<int> nums{1,2,7,6,4};
+    solution(nums);
     return 0; 
 }
