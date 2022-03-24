043fd96adbd36fe150fd9f5554a3b290aeae0c1c
ggh-png <0380089@gmail.com>
2022-03-24 21:34:37 +0900

    수 정렬하기

---
+++ 백준/2750.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> solution(vector<int> arr) 
+{
+    sort(arr.begin(), arr.end());    
+    for(auto el : arr)
+        cout << el << endl;
+    return arr;
+}
+
+int main()
+{
+    int N;
+    cin >> N; 
+    vector<int> arr(N);
+    for(int i=0; i < N; i++)
+        cin >> arr[i];
+
+
+    solution(arr);
+
+    return 0;
+}
