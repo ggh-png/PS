e5d9b001370b4b496b0f51021c6e69e68be4efeb
ggh-png <0380089@gmail.com>
2022-04-04 01:13:41 +0900

    좌표압축

--- 백준/18870.cpp
+++ 백준/18870.cpp
@@ -8,20 +8,13 @@ using namespace std;
 // 좌표 압축 인덱스 부여 
 // 기존 배열과 비교 후 인덱스 값 입력 
 // index : 압축좌표 k : 기존 좌표 begin : 압축좌표의 앞단 end : 압축 좌표의 뒷단
-int BinarySearch(vector<int> index,int k ,int begin ,int end)
-{
-    if(begin > end) return - 1;
-    int m = (begin + end) / 2;
-    if(index[m] == k) return m;
-    else if(index[m] > k) return BinarySearch(index, k, begin, m-1);
-    else return BinarySearch(index, k, m+1, end);
-}
 
 int main()
 {
     int num, temp;
     vector<int> index;
     vector<int> x_list;
+    // 값 입력 
     scanf("%d", &num);
     for(int i=0; i < num; i++)
     {
@@ -29,13 +22,11 @@ int main()
         index.push_back(temp);
         x_list.push_back(temp);
     }
+    // 오름차순 정렬
     sort(index.begin(), index.end());
     // 중복제거 
     index.erase(unique(index.begin(), index.end()), index.end());
     // index 부여
     for(int i=0; i < x_list.size(); i++)
-        x_list[i] = BinarySearch(index, x_list[i], 0, index.size());  
-    for(auto el : x_list)
-        printf("%d ", el);
-    printf("\n");            
+        printf("%ld ", (lower_bound(index.begin(), index.end(), x_list[i]) - index.begin()));      
 }
