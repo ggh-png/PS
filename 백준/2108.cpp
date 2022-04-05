cd93d93d8812be4741952f7a760925fdc1424624
ggh-png <0380089@gmail.com>
2022-04-05 19:40:10 +0900

    통계학

--- 백준/2108.cpp
+++ 백준/2108.cpp
@@ -2,52 +2,61 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+
 using namespace std;
-
-// 산술평균 
-// 중앙값
-// 최빈값 - 여러개일 경우 두번째로 작은 값 ~ counting sort 
-// 최소 최대 범위 
-
-vector<int> solution(vector<int> &count, int num)
-{
-    vector<int> answer;
-
-    // 산술평균
-    double aver=0;
-    for(int i=0; i < count.size(); i++)
-        aver += count[i] * i;
-    answer.push_back(floor((aver / num) + 0.5));
-    // 중앙값
-    int mid = count.size() / 2;
-    for(int i=0; i < count.size(); i++)
-    {
-        if(mid == 0)
-    }
-    answer.push_back(arr[mid]); 
-    // 최빈값 
-    return answer;
-}
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(nullptr);
+    int num, tmp;
+    // 평균
+    double mean = 0;
+    // 중앙, 최빈, 범위
+    int median, mode, range;
 
-    vector<int> count(4001, 0);
-    int num, temp;
-    cin >> num; 
+    vector<int> arr;
+    vector<int> count(8001, 0);
+    cin >> num;
     for(int i=0; i < num; i++)
     {
-        cin >> temp;
-        count[temp] += 1;
-    }    
-
+        cin >> tmp;
+        arr.push_back(tmp);
+        count[tmp + 4000] += 1;
+        mean += tmp;
+    }
+    // 산술평균 
+    mean = floor(mean/num + 0.5);
+    // 중앙 값
+    sort(arr.begin(), arr.end());
+    median = arr[num/2];
+    // 범위 값
+    range = arr.back() - arr.front();
+    // 최빈값
+    int max = -999999; 
+    vector<int> temp; 
+    for(int i=0; i < count.size(); i++)
+        if(count[i] > max)
+            max = count[i];
     
-    for(auto el : solution(count, num))
-        cout << el << " ";
-    cout << endl;
+    for(int i=0; i < count.size(); i++)
+        if(count[i] == max)
+            temp.push_back(i - 4000);
+    // 최빈값이 하나일 경우 
+    if(temp.size() == 1)
+        mode = temp[0];
+    // 여러개일 경우
+    // 최빈값 중 두번째로 작은 값을 최빈값으로 지정   
+    else
+    {
+        sort(temp.begin(), temp.end());
+        temp.erase(unique(temp.begin(), temp.end()), temp.end());
+        mode = temp[1];
+    }
 
+    cout << mean << '\n';
+    cout << median << '\n';
+    cout <<  mode << '\n';
+    cout << range << '\n';
     return 0;
 }
