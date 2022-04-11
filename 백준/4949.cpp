2e431c91ff2e6e8c7f90ec707bc4db20a1127574
ggh-png <0380089@gmail.com>
2022-04-11 22:42:58 +0900

    균형잡힌 세상

--- 백준/4949.cpp
+++ 백준/4949.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 using namespace std;
 
 // 입력 : 공백이 있는 문자열 ... 오로지 '.'만이 문자열의 끝을 알림. 
@@ -17,30 +16,39 @@ int main()
     {
         // 문자열 입력
         getline(cin, str);
+        // stack init
         vector<char> VectorStack; 
         if(str[0] == '.') break;   
         // 판별 ()
-        for(int i=0; i < str.size(); i++)   // 괄호를 입력 받으면 
-        {
+        for(int i=0; i < str.size(); i++)   
+        {   // 괄호를 입력 받으면 
+			// 시작 괄호면 스택쌓기 
             if(str[i] == '(') VectorStack.push_back('(');
             if(str[i] == '[') VectorStack.push_back('[');
             if(str[i] == ')')
-            {
+            {   // 스택이 비어있지 않고 스택의 최 상단에 시작괄호가 있다면 스택제거
+                // () 인 경우 : 빈 스택으로 만듬 
                 if(!VectorStack.empty() && *(VectorStack.end()-1) == '(') 
                     VectorStack.pop_back();
+                // 빈 스택에 닫는괄호가 들어오면 no
+                // ) 인 경우 : no 
                 else{ answer.push_back("no"); break; }    
             }
             if(str[i] == ']')
-            {
+            {   // 스택이 비어있지 않고 스택의 최 상단에 시작괄호가 있다면 스택제거
+                // [] 인 경우 : 빈 스택으로 만듬 
                 if(!VectorStack.empty() && *(VectorStack.end()-1) == '[') 
                     VectorStack.pop_back();
+                // 빈 스택에 닫는괄호가 들어오면 no
+                // ) 인 경우 : no  
                 else{ answer.push_back("no"); break; } 
             }
+            // 스택이 비어있고 .이 나오기 전까지 입력된 문자열을 확인했을 경우  
 			if (VectorStack.empty() && i == str.length() - 2) answer.push_back("yes"); 
-			else if (!VectorStack.empty() && i == str.length() - 2) answer.push_back("no");
+			// 스택이 비어있지 않고 입력된 문자열을 확인했을 경우 
+            else if (!VectorStack.empty() && i == str.length() - 2) answer.push_back("no");
         }   
     }
     for(auto el : answer)
-        cout << el << endl; 
+        cout << el << '\n'; 
 }
-
