aa488de572dd9ecd49794ab740b8ccc5ba5f15ac
ggh-png <0380089@gmail.com>
2022-04-29 12:28:39 +0900

    숫자 카드 2

--- 백준/10816.cpp
+++ 백준/10816.cpp
@@ -22,23 +22,14 @@ int main()
 
     cin >> M_num;
     int answer[M_num];
-    int idx;
+    
     for(int i=0; i < M_num; i++)
     {
         int temp;
         cin >> temp;
         // 이진탐색을 통한 존재유무 탐색 때문에 오름차순 정렬이 필요.   
-        idx = lower_bound(A, A + A_num, temp) - A;
-	
-        // 요소가 존재하면
-        int count = 0;
-        while(temp == A[idx])
-        {
-            count++;
-            // 중복일 시 다음 값
-            idx++;
-        }
-        answer[i] = count;
+        int cnt = upper_bound(A, A + A_num, temp) - lower_bound(A, A + A_num, temp);
+        answer[i] = cnt;
     }
     for(int i=0; i < M_num; i++)
         cout << answer[i] << " ";
