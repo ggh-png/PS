14e687ddafef7448fd9ac46e0ab5c3b08d8c317a
ggh-png <0380089@gmail.com>
2022-05-13 12:12:56 +0900

    별 찍기 - 2

---
+++ 백준/2439.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    for(int i=num; i > 0; i--)
+    {
+        for(int j=1; j < i; j++)
+        {
+            cout << " ";
+        }
+        for(int j=0; j <= num-i; j++)
+            cout << "*";
+        cout << '\n';
+    }
+    return 0;
+}
