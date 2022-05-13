74cb58fbe62f791ff89f60ca03a3c76f4e35dea0
ggh-png <0380089@gmail.com>
2022-05-13 10:28:55 +0900

    빠른 A+B

---
+++ 백준/15552.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, a, b;
+
+    scanf("%d" , &num);
+    for(int i=0; i < num; i++)
+    {
+        scanf("%d %d",&a ,&b);
+        printf("%d\n", a+b);
+    }
+    return 0;
+
+}
