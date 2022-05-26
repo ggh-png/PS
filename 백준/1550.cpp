e9673e475319dad7d470b3989f08136d97ca95c8
ggh-png <0380089@gmail.com>
2022-05-26 14:54:46 +0900

    16진수

---
+++ 백준/1550.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 16진수를 10진수로 바꾸기 
+// A ~ F 
+int main()
+{   // 정수형 - 문자를 넣어도 정수형임 
+    int i;
+    scanf("%x", &i);
+    cout << i;
+}
