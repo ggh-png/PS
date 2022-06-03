c028d81fe0d4273da11e810818564d118a1e062e
ggh-png <0380089@gmail.com>
2022-06-03 12:38:53 +0900

    별 찍기 - 5

---
+++ 백준/2442.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int num;
+    cin >> num;
+    for(int i=num; i > 0; i--)
+    {
+        for(int j=1; j < i; j++)
+            cout << " ";
+        cout << "*";
+        for(int j=0; j < num - i; j++)
+            cout << "*";
+        for(int j=0; j < num - i; j++)
+            cout << "*";
+        cout << endl;
+    }
+    return 0;
+}
