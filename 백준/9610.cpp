3747b4d13039969ede8d16fdb4f3d7408dae929d
ggh-png <71277820+ggh-png@users.noreply.github.com>
2022-08-06 22:53:40 +0900

    Create 9610.cpp
    사분면

---
+++ 백준/9610.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    int arr[5]={0};
+    for(int i=0; i < num; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        if(x > 0 && y > 0)
+            arr[0]++;
+        else if(x < 0 && y > 0)
+            arr[1]++;
+        else if(x < 0 && y < 0)
+            arr[2]++;
+        else if(x > 0 && y < 0)
+            arr[3]++;
+        else
+            arr[4]++;
+     }
+    for(int i=1; i <= 4; i++)
+        cout << "Q" << i << ": " << arr[i-1] << '\n';
+    cout << "AXIS: " << arr[4] << '\n';
+    return 0;
+}
