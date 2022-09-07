8a914bffdf764906c125ec740b8d3737e2741982
ggh-png <0380089@gmail.com>
2022-09-07 10:37:39 +0900

    알고리즘 수업 - 깊이 우선 탐색 1

---
+++ 백준/24479.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> gp[100004];
+bool visited[100004];
+int ans[100004];
+
+int cnt = 0;
+void dfs(int idx)
+{
+    // 방문순서 
+    ans[idx] = ++cnt;
+
+    visited[idx] = 1;
+    for(auto &el : gp[idx])
+        if(!visited[el])
+            dfs(el);
+    return;
+}
+
+int n, m, a, b, srt;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    
+    cin >> n >> m >> srt;
+    for(int i=0; i < m; i++)
+    {
+        cin >> a >> b;
+        // 양 방향 간선이기 떄문에 
+        gp[a].push_back(b);
+        gp[b].push_back(a);
+    }
+
+    for(int i=0; i < n; i++)
+        sort(gp[i].begin(), gp[i].end());
+    dfs(srt);
+    //cout << 0 << '\n';
+    for(int i=1; i <= n; i++)
+        cout << ans[i] << '\n';
+    return 0;
+}
