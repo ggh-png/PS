7d140a3685975aa3105a39f113380ebe459198e0
ggh-png <0380089@gmail.com>
2022-10-26 12:04:43 +0900

    돌 게임 2

---
+++ 백준/9656.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int num;
+    cin >> num;
+    if(num%2)
+        cout << "CY" << endl;
+    else
+        cout << "SK" << endl;
+    return 0;
+}
