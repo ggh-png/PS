969537b1709addde995dcdcc084d5fa526b12b86
ggh-png <0380089@gmail.com>
2022-12-05 12:58:00 +0900

    N과 M (4)

---
+++ 백준/15652.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 서로다른 5개의 구슬이 들어있는 구슬망에서 3개의 구슬을
+// 뽑는 조합 구현 
+// nCr = 5C3
+
+int n, m;
+int arr[9];
+bool visited[9];
+vector<int> v;
+
+void CombiPrint(vector<int> v)
+{
+    for(auto &el : v)
+        cout << el << " ";
+    cout << '\n';
+}
+
+void combi(int start, vector<int> v)
+{
+    // 3개의 구슬이 뽑아지면
+    if(v.size() == m)
+    {
+        CombiPrint(v);
+        return;
+    }
+    for(int i=start; i < n; i++)
+    {
+        v.push_back(arr[i]);
+        combi(i, v);
+        v.pop_back();
+    }
+    return;
+}
+
+int main()
+{
+    cin >> n >> m;
+    for(int i=0; i < n; i++)
+        arr[i] = i+1;
+    combi(0, v);
+    return 0;
+}
