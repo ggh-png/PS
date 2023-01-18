f55f38fdecb5c926a940a0712874837a266eabdc
ggh-png <0380089@gmail.com>
2023-01-18 12:28:09 +0900

    연세대학교

---
+++ 백준/15680.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool flag;
+    cin >> flag;
+
+    if(flag)
+        cout << "Leading the Way to the Future" << endl;
+    else
+        cout << "YONSEI" << endl;
+    return 0;
+}
